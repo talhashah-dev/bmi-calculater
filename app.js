ec5a60554e5736b86fddd38d9d3d448f1633cb23
Syed Talha <syedt.in00@gmail.com>
2023-11-19 23:36:56 +0500

    a button added to reset inputs

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 
 let height = document.getElementById("height-input");
 let weight = document.getElementById("weight-input");
-let calculateBtn = document.getElementById("cal-btn");
 let finalResult = document.getElementById("result");
+let clear = document.getElementById("reset-btn");
+
 
 function calculate(){
     let a = height.value;
     let b = weight.value;
     let result = b / (a*a);
-    
-    if(height.value || weight.value == ""){
-        finalResult.textContent = "Please fill all inputs!"
-    } else  {
-        finalResult.textContent = "Your BMI is " + result.toFixed(1);
-    };
+    finalResult.textContent = "Your BMI is " + result.toFixed(1);
+};
 
+function reset(){
+    height.value = "";
+    weight.value = "";
 };
