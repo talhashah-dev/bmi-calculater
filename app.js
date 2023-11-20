a8fc1a2bbe0cecb08c4b9f116baa4e3c3f6c8d02
Syed Talha <syedt.in00@gmail.com>
2023-11-20 06:15:41 +0500

    bmi based warnings added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let height = document.getElementById("height-input");
 let weight = document.getElementById("weight-input");
 let finalResult = document.getElementById("result");
 let clear = document.getElementById("reset-btn");
+let warning = document.getElementById("warn");
 
 
 function calculate(){
@@ -11,9 +12,26 @@ function calculate(){
     let b = weight.value;
     let result = b / (a*a);
     finalResult.textContent = "Your BMI is " + result.toFixed(1);
+
+    if(result < 18.5 ){
+        warning.style.visibility = "visible";
+        warning.textContent = "You are Underweight";
+    }else if(result > 18.5 && result < 24.9){
+         warning.textContent = "You are Healthy";
+    }else if(result > 25 && result < 29.9){
+         warning.textContent = "You are Overweight";
+    }else if(result > 30 && result < 34.9){
+         warning.textContent = "Obese Class 1";
+    }else if(result > 35 && result < 39.9){
+         warning.textContent = "Obese Class 2";
+    }else if(result > 40){
+         warning.textContent = "Obese Class 3";
+    };
 };
 
 function reset(){
     height.value = "";
     weight.value = "";
+    warning.style.visibility = "hidden";
+    finalResult.textContent = "Press Calculate Button for Result!";
 };
